c36ca57891185490b80ebad41b5d48e7119db3ac
YangSijun528 <yangsijun5528@gmail.com>
2023-06-23 14:08:25 +0900

    first commit

---
+++ src/main/kotlin/dev/yangsijun/rafia/RafiaApplication.kt
@@ -0,0 +1,11 @@
+package dev.yangsijun.rafia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RafiaApplication
+
+fun main(args: Array<String>) {
+    runApplication<RafiaApplication>(*args)
+}
---
+++ src/test/kotlin/dev/yangsijun/rafia/RafiaApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.yangsijun.rafia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RafiaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
