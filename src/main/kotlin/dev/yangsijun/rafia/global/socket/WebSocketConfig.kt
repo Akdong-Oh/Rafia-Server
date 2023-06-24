38e604374f3960233f747b6b7c6889c253d46f8d
YangSijun528 <yangsijun5528@gmail.com>
2023-06-24 23:48:11 +0900

    Update websocket config

--- src/main/kotlin/dev/yangsijun/rafia/global/socket/WebSocketConfig.kt
+++ src/main/kotlin/dev/yangsijun/rafia/global/socket/WebSocketConfig.kt
@@ -1,6 +1,8 @@
 package dev.yangsijun.rafia.global.socket
 
+import dev.yangsijun.rafia.socket.MyChannelInterceptor
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
@@ -11,18 +13,23 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 class WebSocketConfig: WebSocketMessageBrokerConfigurer {
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*")
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*")
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
         registry
             .setApplicationDestinationPrefixes("/pub") // Client에서 websocket 연결 할 api 앤드포인트
             .enableStompBrokerRelay("/topic") // 이 Prefix를 받아서 messageBroker가 해당 방?에 메제지 전달
+//            .setSystemHeartbeatSendInterval(1000)  // 정할수는 있는데, 이거 꺼지는 기준이???
+//            .setSystemHeartbeatReceiveInterval(1000)
             .setRelayHost("localhost")
             .setVirtualHost("/")
-            .setRelayPort(5672)
+            .setRelayPort(61613)
             .setClientLogin("guest")
             .setClientPasscode("guest")
-            //.setHeartbeatValue()
+    }
+
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(MyChannelInterceptor())
     }
 }
