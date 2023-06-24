51e19ef426baf29fe9b5f8c6739d0d76857e844c
YangSijun528 <yangsijun5528@gmail.com>
2023-06-24 23:49:10 +0900

    Game 관련 데이터 수정

--- src/main/kotlin/dev/yangsijun/rafia/data/enums/GameStatus.kt
+++ src/main/kotlin/dev/yangsijun/rafia/data/enums/GameStatus.kt
@@ -1,5 +1,11 @@
 package dev.yangsijun.rafia.data.enums
 
 enum class GameStatus {
-
+    ENTRY,
+    EXIT,
+    EVENT,
+    INTERACTION,
+    CHAT,
+    NOTICE,
+    SET_JOB
 }
--- src/main/kotlin/dev/yangsijun/rafia/data/player/Player.kt
+++ src/main/kotlin/dev/yangsijun/rafia/data/player/Player.kt
@@ -1,13 +1,13 @@
 package dev.yangsijun.rafia.data.player
 
+import dev.yangsijun.rafia.data.enums.GameStatus
 import dev.yangsijun.rafia.data.enums.Skill
+import dev.yangsijun.rafia.data.job.Job
 import dev.yangsijun.rafia.domain.user.domain.User
 
 class Player(
-    val user: User,
-    val id: Int,
-    val job: String? = null, // lazy
+    val user: User, // 기본적으론 UserId를 기준으로 조회/수정
+    val id: String, // sessionId - 연결 끊겼을 때, 빼고는 사용 X
+    val job: Job? = null, // lazy
     val interactions: List<Skill> = listOf()
-) {
-
-}
+)
