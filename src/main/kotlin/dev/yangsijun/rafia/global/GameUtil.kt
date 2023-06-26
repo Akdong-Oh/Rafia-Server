ab511e4e1e4db1118b437a305f4b5ea4dafeab07
YangSijun528 <yangsijun5528@gmail.com>
2023-06-26 12:58:17 +0900

    util 필드 추가 및 분리

---
+++ src/main/kotlin/dev/yangsijun/rafia/global/GameUtil.kt
@@ -0,0 +1,11 @@
+package dev.yangsijun.rafia.global
+
+class GameUtil {
+    companion object {
+        const val NIGHT_TIME: Long = 25
+        const val DAY_TIME_PER_ALIVE_PLAYER: Long = 15
+        const val VOTE_TIME: Long = 15
+        const val OBJECTION_TIME: Long = 15
+        const val CONFIRMATION_TIME: Long = 10
+    }
+}
--- src/main/kotlin/dev/yangsijun/rafia/global/RoomUtil.kt
+++ src/main/kotlin/dev/yangsijun/rafia/global/RoomUtil.kt
@@ -1,6 +1,6 @@
 package dev.yangsijun.rafia.global
 
-class Util {
+class RoomUtil {
     companion object {
         const val MAX_ROOM_PLAYER : Int = 8
         const val MIN_ROOM_START_PLAYER : Int = 6
